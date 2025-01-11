b963990873076a1081a40397eeb96ac1c2bf0e69
Aleksey Sakhonchyk <wotalcatras@gmail.com>
2025-01-11 19:31:59 +0300

    Change files

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
+        Task1();
+        Task2();
+        Task3();
+    }
+
+    public static void Task1() {
         System.out.println("Enter 2 numbers separated by a space");
 
         Scanner scanner = new Scanner(System.in);
@@ -17,22 +21,40 @@ public class Main
 
         DecimalFormat df = new DecimalFormat("0.##");
 
-        if (num1 >  num2)
-        {
+        if (num1 > num2) {
             System.out.println(df.format(num1) + " > " + df.format(num2));
-        }
-        else if (num1 < num2)
-        {
+        } else if (num1 < num2) {
             System.out.println(df.format(num1) + " < " + df.format(num2));
-        }
-        else
-        {
+        } else {
             System.out.println(df.format(num1) + " = " + df.format(num2));
         }
 
-        System.out.println(df.format(num1) + " + " + df.format(num2) + " = " + df.format(num1 + num2) +"\n"
-                + df.format(num1) + " - " + df.format(num2) + " = " + df.format(num1 - num2) +"\n"
-                + df.format(num1) + " * " + df.format(num2) + " = " + df.format(num1 * num2) +"\n"
-                + df.format(num1) + " / " + df.format(num2) + " = " + df.format(num1 / num2) +"\n");
+        System.out.println(df.format(num1) + " + " + df.format(num2) + " = " + df.format(num1 + num2) + "\n"
+                + df.format(num1) + " - " + df.format(num2) + " = " + df.format(num1 - num2) + "\n"
+                + df.format(num1) + " * " + df.format(num2) + " = " + df.format(num1 * num2) + "\n"
+                + df.format(num1) + " / " + df.format(num2) + " = " + df.format(num1 / num2) + "\n");
+    }
+
+    public static void Task2() {
+        System.out.println("Enter the first line");
+        Scanner scanner = new Scanner(System.in);
+        String firstlineinput = scanner.nextLine();
+        System.out.println("Enter the second line");
+        String secondlineinput = scanner.nextLine();
+
+        if (firstlineinput.equals(secondlineinput)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
         }
     }
+
+    public static void Task3() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+    }
+}
